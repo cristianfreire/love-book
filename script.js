5e1dfc594fe3cbb42be1e1c0a2269b8263e6933d
Cristian Passos <cristianpassos51@gmail.com>
2025-03-21 11:26:59 -0300

    Add 5 paginas e flip corrigido

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const page3 = document.getElementById('page3');
 const page4 = document.getElementById('page4');
+const page5 = document.getElementById('page5');
 
 let currentPage = 0;
 const totalPages = 6;
@@ -17,9 +18,9 @@ page0.addEventListener('click', function (event) {
     console.log('Clique à esquerda');
   } else {
     if (currentPage <= totalPages) {
-      console.log(`page${currentPage}`);
       document.getElementById(`page${currentPage}`).classList.toggle("flip");
       currentPage++;
+      console.log(`page${currentPage}`);
     }
 
   }
@@ -39,9 +40,9 @@ page1.addEventListener('click', function (event) {
     }
   } else {
     if (currentPage <= totalPages) {
-      console.log(`page${currentPage}`);
-      document.getElementById(`page${currentPage}`).classList.add("flip");
+      document.getElementById(`page${currentPage}`).classList.toggle("flip");
       currentPage++;
+      console.log(`page${currentPage}`);
     }
   }
 });
@@ -56,14 +57,15 @@ page2.addEventListener('click', function (event) {
   const middle = storyRect.left + storyRect.width / 2;
 
   if (clickPosition < middle) {
-    console.log('Clique à esquerda');
-    currentPage--;
-    document.getElementById(`page${currentPage}`).classList.toggle("flip");
-
+    if (currentPage <= totalPages) {
+      console.log('Clique à esquerda');
+      currentPage--;
+      document.getElementById(`page${currentPage}`).classList.toggle("flip");
+    }
   } else {
-    console.log(`page${currentPage}`);
-    document.getElementById(`page${currentPage}`).classList.add("flip");
+    document.getElementById(`page${currentPage}`).classList.toggle("flip");
     currentPage++;
+    console.log(`page${currentPage}`);
   }
 });
 
@@ -76,14 +78,15 @@ page3.addEventListener('click', function (event) {
   const middle = storyRect.left + storyRect.width / 2;
 
   if (clickPosition < middle) {
-    console.log('Clique à esquerda');
-    currentPage--;
-    document.getElementById(`page${currentPage}`).classList.toggle("flip");
-
+    if (currentPage <= totalPages) {
+      console.log('Clique à esquerda');
+      currentPage--;
+      document.getElementById(`page${currentPage}`).classList.toggle("flip");
+    }
   } else {
-    console.log(`page${currentPage}`);
-    document.getElementById(`page${currentPage}`).classList.add("flip");
+    document.getElementById(`page${currentPage}`).classList.toggle("flip");
     currentPage++;
+    console.log(`page${currentPage}`);
   }
 });
 
@@ -96,13 +99,35 @@ page4.addEventListener('click', function (event) {
   const middle = storyRect.left + storyRect.width / 2;
 
   if (clickPosition < middle) {
-    console.log('Clique à esquerda');
-    currentPage--;
+    if (currentPage <= totalPages) {
+      console.log('Clique à esquerda');
+      currentPage--;
+      document.getElementById(`page${currentPage}`).classList.toggle("flip");
+    }
+  } else {
     document.getElementById(`page${currentPage}`).classList.toggle("flip");
+    currentPage++;
+    console.log(`page${currentPage}`);
+  }
+});
+
+// pg5
+page5.addEventListener('click', function (event) {
+  const storyRect = page5.getBoundingClientRect(); // Pega a posição da div
+  const clickPosition = event.clientX; // Pega a posição do clique na horizontal
+
+  // Calcula o meio da div
+  const middle = storyRect.left + storyRect.width / 2;
+
+  if (clickPosition < middle) {
+    console.log('Clique à esquerda');
+    if (currentPage <= totalPages) {
+      console.log('Clique à esquerda');
+      currentPage--;
+      document.getElementById(`page${currentPage}`).classList.toggle("flip");
+    }
 
   } else {
-    // console.log(`page${currentPage}`);
-    // document.getElementById(`page${currentPage}`).classList.add("flip");
-    // currentPage++;
+    console.log(`page${currentPage}`);
   }
 });
